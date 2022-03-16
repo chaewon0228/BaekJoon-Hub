8308651e2eed9d7250edf458f1b0f443fe320f42
Yun ChaeWon <58872725+chaewon0228@users.noreply.github.com>
2022-03-16 21:31:58 +0900

    [Bronze I] Title: 더하기 사이클, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int a= sc.nextInt();
+        int num= a;
+        int count=0;
+        int t,o;
+        do{
+            t= num/10;
+            o= num - 10*t;
+            
+            num= 10*o + ((t+o) % 10);
+            count++;
+        }while(num!=a);
+        System.out.println(count);
+        sc.close();
+    }
+}
