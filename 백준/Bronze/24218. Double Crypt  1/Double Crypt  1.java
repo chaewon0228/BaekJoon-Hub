1b24d332d9e43c54f592f7353e21a3e67427ecb9
Yun ChaeWon <58872725+chaewon0228@users.noreply.github.com>
2022-03-21 16:34:25 +0900

    [Bronze V] Title: Double Crypt  1, Time: 204 ms, Memory: 17564 KB -BaekjoonHub

---
+++ 백준/Bronze/24218. Double Crypt  1/Double Crypt  1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+        long s = sc.nextLong();
+        String a = sc.next();
+        String b = sc.next();
+
+        System.out.println("A0000000000000000000000000000000");
+        System.out.print("70000000000000000000000000000000");
+		sc.close();
+	}
+}
