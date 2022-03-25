53c083ff89491cdd2a0e51175861357945dee692
Yun ChaeWon <58872725+chaewon0228@users.noreply.github.com>
2022-03-25 23:44:32 +0900

    [Silver V] Title: 소트인사이드, Time: 228 ms, Memory: 18376 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] cnt = new int[10];
+        
+        while(n != 0){
+            cnt[n % 10]++;
+            n /= 10;
+        }
+        for (int i=9; i>=0; i--) {
+			while (cnt[i]-- > 0) {
+				System.out.print(i);
+			}
+		}
+    }
+}
