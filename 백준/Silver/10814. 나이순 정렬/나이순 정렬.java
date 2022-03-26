7510ebdc4a0c07e424d06f998e22d9f5d57d2677
Yun ChaeWon <58872725+chaewon0228@users.noreply.github.com>
2022-03-26 23:53:46 +0900

    [Silver V] Title: 나이순 정렬, Time: 2392 ms, Memory: 109000 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
+ 
+public class Main {
+	public static void main(String[] args) {		
+        Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String[][] an = new String[n][2];
+		for(int i=0; i<n; i++) {
+			an[i][0] = sc.next(); // 나이
+			an[i][1] = sc.next(); // 이름
+		}
+        Arrays.sort(an, new Comparator<String[]>() {
+			public int compare(String[] s, String[] s2) {
+				return Integer.parseInt(s[0])-Integer.parseInt(s2[0]);
+			}
+		});
+		
+		for(int i=0; i<n; i++) {
+			System.out.println(an[i][0]+" "+an[i][1]);
+		}
+    }
+}
