26449afd835364be0bddf508c592832af97b8b85
Yun ChaeWon <58872725+chaewon0228@users.noreply.github.com>
2022-04-04 23:48:53 +0900

    [Silver V] Title: 배열 합치기, Time: 1872 ms, Memory: 348620 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			list.add(Integer.parseInt(st.nextToken()));
+		}
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<m; i++) {
+			list.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		Collections.sort(list);
+		for(int i=0; i<list.size(); i++) {
+			bw.write(list.get(i) + " ");
+		}
+		bw.flush();
+	}
+}
+
