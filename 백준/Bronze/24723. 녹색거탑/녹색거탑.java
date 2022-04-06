431b6a6eef17cc7313ebcf2cbe6f8f372fc79da8
Yun ChaeWon <58872725+chaewon0228@users.noreply.github.com>
2022-04-06 09:48:47 +0900

    [Bronze IV] Title: 녹색거탑, Time: 200 ms, Memory: 17568 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int cnt = 1;
+		System.out.println(cnt << n); // 비트 연산
+	}
+}
