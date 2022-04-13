cc895ec757dc017e4406fd2273d0fd2bef81752c
Yun ChaeWon <58872725+chaewon0228@users.noreply.github.com>
2022-04-13 15:05:30 +0900

    [Bronze I] Title: 팰린드롬수, Time: 196 ms, Memory: 17528 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = "";
+		boolean result;
+        
+		while(true) {
+			str = sc.next();
+			result = true;
+			if(str.equals("0")) break;
+			
+			int len = str.length();
+			for(int i=0; i<str.length() / 2; i++) {
+				if(str.charAt(i) != str.charAt(len - 1 - i)) {
+					result = false;
+				}
+			}
+			
+			if(result) System.out.println("yes");
+			else System.out.println("no");
+		}
+	}
+}
